a58d1e462f14cdf677b141cae9648fee7faad4fe
Greed <jaiimmortal@gmail.com>
2017-07-22 10:10:03 +0530

    Removed unnecessary functions

--- Async/ydlcelery.py
+++ Async/ydlcelery.py
@@ -4,25 +4,9 @@
 # In[4]:
 
 import subprocess
-import os
 from celery import Celery
-import requests
-from bs4 import BeautifulSoup
-from fetcher import id_generator
 
 app = Celery('tasks', backend='amqp', broker='amqp://')
-
-def fetch_name(url):
-
-    youtube_page = requests.get(url)
-    bsObj = BeautifulSoup(youtube_page.text, 'html.parser')
-    return bsObj.title.text
-
-def verify(url):
-
-    if 'www.youtube.com' in url or 'youtu.be' in url :
-        return True
-    return False
 
 @app.task
 def audio(url):
